f6d4ee4811974f0e795351adc2ba9b00e9abe213
ssamal1 <ssamal@ucsd.edu>
2024-02-14 11:54:28 -0800

    Grader

--- grade.sh
+++ grade.sh
@@ -14,3 +14,42 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+
+cp student-submission/*.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [ -f ListExamples.java ]
+then
+    echo "Missing ListExamples.java"
+    echo "Score: 0"
+    exit
+fi
+
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]
+then 
+    echo "Compilation Error"
+    echo "Score: 0"
+    exit
+fi
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+tests=$(java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep 'Failures' | cut -d' ' -f3 | cut -c 1)
+failures=$(java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep 'Failures' | cut -d' ' -f6)
+
+if [[ -n "$tests" ]]
+then 
+    echo "Score: $(( $tests - $failures / $tests ))"
+
+
+else 
+    echo "Score: 100.0"
+
+fi
